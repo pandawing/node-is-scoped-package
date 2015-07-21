4b1e39858a4a5c2fe5d0348290658167b2f377dd
sanemat <o.gata.ken@gmail.com>
2015-07-21 12:03:40 +0900

    test(scoped): add test patterns

--- test.js
+++ test.js
@@ -2,8 +2,14 @@
 var assert = require('power-assert');
 var isScopedPackage = require('./');
 
-it('should not be scoped package', function () {
+it('should not be scoped package, normal package', function () {
   assert.equal(isScopedPackage('generator-nm'), false);
+});
+it('should not be scoped package, github pattern', function () {
+  assert.equal(isScopedPackage('sanemat/generator-nm'), false);
+});
+it('should not be scoped package, invalid', function () {
+  assert.equal(isScopedPackage('@/generator-nm'), false);
 });
 it('should be scoped package', function () {
   assert.equal(isScopedPackage('@sanemat/generator-nm'), true);
